6135d8cb1d4bea2ceec9fb8fe12572a00ba5c591
German Fonseca <gfonseca0193@gmail.com>
2020-05-29 21:30:33 -0500

    Added theory tests

--- Calculator.Tests/Calculator.Test.cs
+++ Calculator.Tests/Calculator.Test.cs
@@ -16,38 +16,35 @@ namespace Tests
         {
              var result = _calculator.Add(1,1);
              Assert.Equal(2, result);
+             Assert.Equal(5,_calculator.Add(6,-1));
         }
 
-        [Fact]
-        public void TestSubtract()
+        [Theory]
+        [InlineData(8,3,5)]
+        [InlineData(-1,-2,1)]
+        [InlineData(5,-2,7)]
+        [InlineData(-7,2,-9)]
+        public void TestSubtract(int value1, int value2, int expected)
         {
-        //Given
-            var result = _calculator.Subtract(3,-1);
-        //When
-        
-        //Then
-            Assert.Equal(4,result);
+            var result = _calculator.Subtract(value1,value2);
+            Assert.Equal(expected,result);
         }
 
-        [Fact]
-        public void TestMultiply()
+        [Theory]
+        [InlineData(3,5,15)]
+        [InlineData(10,0,0)]
+        [InlineData(-3,2,-6)]
+        [InlineData(-5,-2,10)]
+        public void TestMultiply(int value1, int value2, int expected)
         {
-        //Given
-            var result = _calculator.Multiply(2,4);
-        //When
-        
-        //Then
-            Assert.Equal(8,result);
+            var result = _calculator.Multiply(value1,value2);
+            Assert.Equal(expected,result);
         }
 
         [Fact]
         public void TestDivide()
         {
-        //Given
             var result = _calculator.Divide(5,2);
-        //When
-        
-        //Then
             Assert.Equal(2.5,result);
         }
     }
